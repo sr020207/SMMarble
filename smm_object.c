87e26c74dc224a674d38a33d6f7d0dea7806c9a2
sr020207 <144992835+sr020207@users.noreply.github.com>
2023-11-30 16:15:05 +0900

    231130

---
+++ smm_object.c
@@ -0,0 +1,91 @@
+//
+//  smm_node.c
+//  SMMarble
+//
+//  Created by Juyeop Kim on 2023/11/05.
+//
+
+#include "smm_common.h"
+#include "smm_object.h"
+#include <string.h>
+
+#define MAX_NODETYPE    7
+#define MAX_GRADE       9
+#define MAX_NODE        100
+
+
+static char smmNodeName[SMMNODE_TYPE_MAX][MAX_CHARNAME] = {
+       "????",
+       "???",
+       "?????",
+       "??",
+       "?????????",
+       "????????",
+       "?????©£?"
+};
+
+char* smmObj_getTypeName(int type)
+{
+      return (char*)smmNodeName[type];
+}
+
+//1. ????u ???? ????
+typedef struct smmObject {
+       char name[MAX_CHARNAME];
+       int type;
+       int credit;
+       int energy;
+} smmObject_t;
+
+//2. ????u ?ò÷ ???? ???? 
+smmObject_t smm_node[MAX_NODE];
+
+#if 0
+static char smmObj_name[MAX_NODE][MAX_CHARNAME];
+static int smmObj_type[MAX_NODE];
+static int smmObj_credit[MAX_NODE];
+static int smmObj_energy[MAX_NODE];
+#endif
+
+static int smmObj_noNode = 0;
+
+//3. ???? ??? ???? 
+//object generation
+void smmObj_genNode(char* name, int type, int credit, int energy)
+{
+    #if 0
+    strcpy(smmObj_name[smmObj_noNode], name);
+    smmObj_type[smmObj_noNode] = type;
+    smmObj_credit[smmObj_noNode] = credit;
+    smmObj_energy[smmObj_noNode] = energy;
+    #endif
+    
+    strcpy(smm_node[smmObj_noNode].name, name);
+    smm_node[smmObj_noNode].type = type;
+    smm_node[smmObj_noNode].credit = credit;
+    smm_node[smmObj_noNode].energy = energy;
+    
+    smmObj_noNode++;
+}
+
+//3. ???? ??? ???? 
+char* smmObj_getNodeName(int node_nr)
+{
+    return smm_node[node_nr].name;
+}
+
+//3. ???? ??? ???? 
+int smmObj_getNodeType(int node_nr)
+{
+    return smm_node[node_nr].type;
+}
+
+int smmObj_getNodeCredit(int node_nr)
+{
+    return smm_node[node_nr].credit;
+}
+
+int smmObj_getNodeEnergy(int node_nr)
+{
+    return smm_node[node_nr].energy;
+}
